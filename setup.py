f2eca20001f10831bbdd497292fef333d7e349c1
Jeff Kinnison <jkinniso@nd.edu>
2016-04-08 18:07:40 -0400

    Update to setup.py

--- setup.py
+++ setup.py
@@ -9,5 +9,11 @@ from disutils.core import setup
 setup(
     name="simstream",
     version="0.1dev",
-    packages=["tornado"],
+    author="Jeff Kinnison",
+    author_email="jkinniso@nd.edu",
+    packages=["simstream"],
+    description=""
+    install_requires=[
+        "tornado >= 4.3"
+    ],
 )
