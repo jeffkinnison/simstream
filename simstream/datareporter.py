a75f936c036d70a4dcd6d18fd2186f804b24d4a3
Jeff Kinnison <jkinniso@nd.edu>
2016-04-08 18:34:25 -0400

    Added first stage of interval data collection

--- simstream/datareporter.py
+++ simstream/datareporter.py
@@ -4,6 +4,7 @@ Utilties for collecting system data.
 Author: Jeff Kinnison (jkinniso@nd.edu)
 """
 
+import threading
 
 from . import datacollector
 
@@ -13,20 +14,30 @@ class DataReporter(object):
 
     def __init__(self, interval=1000, **kwargs):
         self.interval = interval
-        self.resources = {}
+        self.collectors = {}
         for key, value in kwargs:
-            self.resources[key] = datacollector.DataCollector(
+            self.collectors[key] = datacollector.DataCollector(
                 key,
                 value.callback,
                 value.postprocessor,
                 value.args
             )
 
+    def run(self):
+        self._collection_event = threading.Event()
+        while not self._collection_event.wait(timeout=self.interval):
+            self._record_resources()
+
+
+
+    def stop(self):
+        self._collection_event.set()
+
     def __getitem__(self, name):
         """Return the data from the collector specified by name"""
         return self.resources[name].data
 
-    def _record_resources(self, resource, value):
+    def _record_resources(self):
         """Run all collectors"""
         for key, value in self.resources:
             value()
