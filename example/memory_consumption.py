737dd42e30eafec81f825cba9e22acbc916fb22b
Jeff Kinnison <jkinniso@nd.edu>
2016-04-09 16:16:21 -0400

    Made memory usage example more usable

--- example/memory_consumption.py
+++ example/memory_consumption.py
@@ -1,5 +1,7 @@
 import simstream
 import resource
+import tornado.web
+import tornado.ioloop
 
 
 def mem_callback():
@@ -9,14 +11,22 @@ def mem_callback():
 def mem_postprocessor(rss):
     return rss / 1000
 
-mem_reporter = simstream.DataReporter(3)
-mem_reporter.add_collector("rss",
-                           limit=100,
-                           callback=mem_callback,
-                           postprocessor=mem_postprocessor
-                           )
+if __name__ == "__main__":
+    mem_reporter = simstream.DataReporter(3)
+    mem_reporter.add_collector("rss",
+                               limit=100,
+                               callback=mem_callback,
+                               postprocessor=mem_postprocessor
+                               )
 
-mem_handler = simstream.ReporterHandler(reporter=mem_reporter,
-                                        template="mem_template.html")
+    mem_handler = simstream.ReporterHandler(reporter=mem_reporter,
+                                            template="mem_template.html")
 
-mem_streamer = simstream.Streamer()
+    mem_streamer = simstream.Streamer()
+
+    mem_streamer.add_handlers([
+        (r"/mem/(.*)", mem_handler)
+    ])
+
+    mem_streamer.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
