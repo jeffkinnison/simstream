4c05d7093e7f646f1b7450063587ba0588c6d80b
Jeff Kinnison <jeffdkinnison@gmail.com>
2016-05-27 16:10:45 -0400

    updated to include new files and classes

--- simstream/__init__.py
+++ simstream/__init__.py
@@ -4,6 +4,8 @@ Utilties for collecting and distributing system data.
 Author: Jeff Kinnison (jkinniso@nd.edu)
 """
 
-from .streamer import Streamer, ReporterHandler
+from .simstream import SimStream
 from .datareporter import DataReporter, CollectorExistsException, CollectorDoesNotExistException
 from .datacollector import DataCollector
+from .pikaasyncconsumer import PikaAsyncConsumer
+from .pikaproducer import PikaProducer
